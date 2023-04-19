2215d45cccde24f275870b97d45444159ec2ec8e
meretamal <meretamal@uc.cl>
2023-04-19 15:26:07 -0400

    chore: setup library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), vueJsx()],
+  build: {
+    lib: {
+      entry: [resolve(__dirname, 'src', 'index.ts')],
+      name: 'Celeste',
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+});
