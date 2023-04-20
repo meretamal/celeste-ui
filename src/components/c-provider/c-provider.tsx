4c4664d99cde09c2d376101d78f56ce05f22365c
meretamal <meretamal@uc.cl>
2023-04-20 11:36:50 -0400

    feat(c-provider): allow passing a custom theme

--- src/components/c-provider/c-provider.tsx
+++ src/components/c-provider/c-provider.tsx
@@ -1,9 +1,19 @@
-import { defineComponent, provide } from 'vue';
+import { PropType, defineComponent, provide, computed, watch } from 'vue';
+import merge from 'lodash/merge';
 import { baseTheme } from '../../theme';
+import { Theme, DeepPartial } from '../../types';
 
 export const CProvider = defineComponent({
-  setup(_, { slots }) {
-    provide('theme', baseTheme);
+  props: {
+    theme: {
+      type: Object as PropType<DeepPartial<Theme>>,
+      default: null,
+    },
+  },
+  setup(props, { slots }) {
+    const theme = computed<Theme>(() => merge(baseTheme, props.theme));
+    watch(theme, () => provide('theme', theme.value));
+
     return () => slots.default?.();
   },
 });
