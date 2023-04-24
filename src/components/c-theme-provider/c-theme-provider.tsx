571557e2e65a079083030515b228bbfafa5dce98
meretamal <meretamal@uc.cl>
2023-04-24 17:58:37 -0400

    fix(c-provider): fix theme not being reactive

--- src/components/c-theme-provider/c-theme-provider.tsx
+++ src/components/c-theme-provider/c-theme-provider.tsx
@@ -1,9 +1,6 @@
-import { PropType, defineComponent, provide, computed, readonly } from 'vue';
+import { PropType, defineComponent, provide, computed } from 'vue';
 import merge from 'lodash/merge';
 import { baseTheme } from '@/theme';
-import { useButtonStyles } from '@/components/c-button/c-button.styles';
-import { useTextFieldStyles } from '@/components/c-text-field/c-text-field.styles';
-import { useTypographyStyles } from '@/components/c-typography/c-typography.styles';
 import { Theme, DeepPartial } from '@/types';
 import { CCSSBaseline } from '@/components/c-css-baseline';
 
@@ -17,11 +14,7 @@ export const CThemeProvider = defineComponent({
   },
   setup(props, { slots }) {
     const theme = computed<Theme>(() => merge(baseTheme, props.theme));
-    provide('theme', readonly(theme));
-
-    useTypographyStyles();
-    useButtonStyles();
-    useTextFieldStyles();
+    provide('theme', theme);
     return () => <CCSSBaseline>{slots.default?.()}</CCSSBaseline>;
   },
 });
