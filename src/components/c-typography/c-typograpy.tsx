b5a728e3f4155c3377e4d69fb92337e5966ff430
meretamal <meretamal@uc.cl>
2023-04-23 22:09:41 -0400

    feat(c-typography): handle variant styles

--- src/components/c-typography/c-typograpy.tsx
+++ src/components/c-typography/c-typograpy.tsx
@@ -1,5 +1,6 @@
 import { PropType, computed, defineComponent } from 'vue';
 import { ElementType } from '@polymorphic-factory/vue';
+import { css } from '@emotion/css';
 import { celeste } from '../../celeste';
 
 export const CTypography = defineComponent({
@@ -29,9 +30,51 @@ export const CTypography = defineComponent({
     };
 
     const tag = computed(() => props.as || defaultMapping[props.variant]);
-    // eslint-disable-next-line react/jsx-pascal-case
+
+    const baseClass = computed(() =>
+      css({
+        '&--h1': {
+          fontSize: '3rem',
+          lineHeight: '3.5rem',
+        },
+        '&--h2': {
+          fontSize: '2.25rem',
+          lineHeight: '2.75rem',
+        },
+        '&--h3': {
+          fontSize: '1.75rem',
+          lineHeight: '2.25rem',
+        },
+        '&--h4': {
+          fontSize: '1.625rem',
+          lineHeight: '2.125rem',
+        },
+        '&--h5': {
+          fontSize: '1.5rem',
+          lineHeight: '2rem',
+        },
+        '&--h6': {
+          fontSize: '1.375rem',
+          lineHeight: '1.875rem',
+        },
+        '&--b1': {
+          fontSize: '1.125rem',
+          lineHeight: '1.35rem',
+        },
+        '&--b2': {
+          fontSize: '1rem',
+          lineHeight: '1.175rem',
+        },
+      }),
+    );
+    const classes = computed(() => [
+      baseClass.value,
+      `${baseClass.value}--${props.variant}`,
+    ]);
     return () => (
-      <celeste.span as={tag.value}>{slots.default?.()}</celeste.span>
+      <celeste.span as={tag.value} class={classes.value}>
+        {slots.default?.()}
+      </celeste.span>
     );
   },
 });
