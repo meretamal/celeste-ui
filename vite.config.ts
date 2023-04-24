63845147845ff9a75d9be5787737ead2099c7b32
meretamal <meretamal@uc.cl>
2023-04-24 09:58:48 -0400

    chore: setup src import alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, '/src') }],
+  },
   plugins: [vue(), vueJsx()],
   build: {
     lib: {
