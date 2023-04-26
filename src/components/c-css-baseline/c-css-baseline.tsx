f2b8f40e82ff8677f8940dff8588a07048fdc869
meretamal <meretamal@uc.cl>
2023-04-26 18:42:01 -0400

    feat(c-css-baseline): provide component styles to children

--- src/components/c-css-baseline/c-css-baseline.tsx
+++ src/components/c-css-baseline/c-css-baseline.tsx
@@ -1,5 +1,10 @@
 import { defineComponent } from 'vue';
 import { injectGlobal } from '@emotion/css';
+import { useButtonStyles } from '@/components/c-button/c-button.styles';
+import { useCheckboxStyles } from '../c-checkbox/c-checkbox.styles';
+import { useTextFieldStyles } from '../c-text-field/c-text-field.styles';
+import { useTypographyStyles } from '../c-typography/c-typography.styles';
+import { useSelectStyles } from '../c-select/c-select.styles';
 
 export const CCSSBaseline = defineComponent({
   name: 'CCSSBaseline',
@@ -12,6 +17,11 @@ export const CCSSBaseline = defineComponent({
         fontFamily: "'Lato', sans-serif",
       },
     });
+    useButtonStyles();
+    useCheckboxStyles();
+    useTextFieldStyles();
+    useTypographyStyles();
+    useSelectStyles();
     return () => slots.default?.();
   },
 });
