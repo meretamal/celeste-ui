808325b6f1bc3a606d39285fc011f0c27fe213b3
meretamal <meretamal@uc.cl>
2023-05-01 17:23:14 -0400

    refactor(c-typography): reduse duplicated code

--- src/components/c-typography/c-typography.spec.tsx
+++ src/components/c-typography/c-typography.spec.tsx
@@ -1,67 +1,66 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/vue';
+import { render, screen, RenderOptions } from '@testing-library/vue';
 import { CTypography } from './c-typograpy';
+
+const renderComponent = (props?: RenderOptions['props']) =>
+  render(<CTypography {...props}>Hello world</CTypography>);
 
 describe('CTypography', () => {
   it('should render the given text', () => {
-    render(<CTypography>Hello world</CTypography>);
+    renderComponent();
     screen.getByText('Hello world');
   });
 
   it('should render the correct HTML tag for and h1 variant', () => {
-    render(<CTypography variant="h1">Hello world</CTypography>);
+    renderComponent({ variant: 'h1' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H1');
   });
 
   it('should render the correct HTML tag for and h2 variant', () => {
-    render(<CTypography variant="h2">Hello world</CTypography>);
+    renderComponent({ variant: 'h2' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H2');
   });
 
   it('should render the correct HTML tag for and h3 variant', () => {
-    render(<CTypography variant="h3">Hello world</CTypography>);
+    renderComponent({ variant: 'h3' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H3');
   });
 
   it('should render the correct HTML tag for and h4 variant', () => {
-    render(<CTypography variant="h4">Hello world</CTypography>);
+    renderComponent({ variant: 'h4' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H4');
   });
 
   it('should render the correct HTML tag for and h5 variant', () => {
-    render(<CTypography variant="h5">Hello world</CTypography>);
+    renderComponent({ variant: 'h5' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H5');
   });
 
   it('should render the correct HTML tag for and h6 variant', () => {
-    render(<CTypography variant="h6">Hello world</CTypography>);
+    renderComponent({ variant: 'h6' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('H6');
   });
 
   it('should render the correct HTML tag for and b1 variant', () => {
-    render(<CTypography variant="b1">Hello world</CTypography>);
+    renderComponent({ variant: 'b1' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('P');
   });
 
   it('should render the correct HTML tag for and b2 variant', () => {
-    render(<CTypography variant="b2">Hello world</CTypography>);
+    renderComponent({ variant: 'b2' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('P');
   });
 
   it('should render a custom tag if the "as" prop is provided', () => {
-    render(
-      <CTypography variant="h1" as="span">
-        Hello world
-      </CTypography>,
-    );
+    renderComponent({ variant: 'h1', as: 'span' });
     const element = screen.getByText('Hello world');
     expect(element.tagName).toBe('SPAN');
   });
